a88872db645d0b361e56209ecac28e3cc9cdf543
sphsconormurphy <8cmurphy@stpatrick.org>
2016-10-06 14:10:32 -0500

    Thursday afternoon working on Octocat object

---
+++ octocats.js
@@ -0,0 +1,21 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function Octocat(name, arms){
+  this.n = name;
+  this.a = arms;
+
+  var slap = for(var x=0;x<this.a;x++){
+    console.log("SLAP");
+  };
+}
+
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
